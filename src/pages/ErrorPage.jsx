f423dd9fd5a23835022abe8c9b89d5d83b2b11ec
dawidtt <dawid.gorszka@gmail.com>
2025-03-28 02:00:40 +0100

    Center Error page text

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 function ErrorPage() {
   return (
-    <div className="w-[100v] h-[100vh] flex items-center justify-center flex-col">
+    <div className="w-[100v] h-[100vh] flex items-center justify-center flex-col text-center">
       <h1 className="text-2xl font-bold">
         Oh no, you're lost. This site doesn't exist!
       </h1>
