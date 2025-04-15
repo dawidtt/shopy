cef835284f274a70a831582fafa2eb6cc240570e
dawidtt <dawid.gorszka@gmail.com>
2025-04-15 20:07:02 +0200

    Start to create ProductPage

---
+++ src/pages/Shop/ProductPage.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+function ProductPage({ title, description, price, rating }) {
+  return (
+    <div>
+      <Header></Header>
+      <div></div>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default ProductPage;
